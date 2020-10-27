3b64e05bdb768754f4f215bf3080e1030af55fc6
gadget <g4dg3t@gmail.com>
2020-10-27 19:09:37 +0100

    plain html chat client

--- src/main/java/com/paperboy/example/PaperboyAuthController.java
+++ src/main/java/com/paperboy/example/PaperboyAuthController.java
@@ -2,9 +2,7 @@ package com.paperboy.example;
 
 import com.paperboy.connector.PaperboyConnector;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -17,6 +15,11 @@ public class PaperboyAuthController {
     @GetMapping(path = "/paperboyAuth/{channel}")
     public String requestToken(Principal principal, @PathVariable String channel) {
         return paperboyConnector.requestToken(principal, channel);
+    }
+
+    @PostMapping(path = "/sendMessage/{channel}")
+    public void sendMessage(@PathVariable String channel, @RequestBody String message) {
+        paperboyConnector.sendToChannel(channel, new SimpleMessage(message));
     }
 
 }
