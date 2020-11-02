21dbb534c9e7e51276f673fc28ac5a1828b593ec
gadget <g4dg3t@gmail.com>
2020-11-02 20:00:11 +0100

    minor changes

--- src/main/java/com/paperboy/example/PaperboyController.java
+++ src/main/java/com/paperboy/example/PaperboyController.java
@@ -2,6 +2,7 @@ package com.paperboy.example;
 
 import com.paperboy.connector.PaperboyConnector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -12,13 +13,16 @@ public class PaperboyController {
     @Autowired
     private PaperboyConnector paperboyConnector;
 
+    @Secured("ROLE_USER")
     @GetMapping(path = "/paperboyAuth/{channel}")
     public String requestToken(Principal principal, @PathVariable String channel) {
-        return paperboyConnector.requestToken(principal, channel);
+        return paperboyConnector.generateToken(principal, channel);
     }
 
+    @Secured("ROLE_USER")
     @PostMapping(path = "/sendMessage/{channel}")
     public void sendMessage(Principal principal, @PathVariable String channel, @RequestBody String message) {
+        // TODO: if authorization is needed for publishers, it needs to be implemented here as part of the application backend
         paperboyConnector.sendToChannel(channel, new ChatMessage(principal.getName(), message));
     }
 
